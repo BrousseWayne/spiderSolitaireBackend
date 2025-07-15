fa5053e733171268d31520ca8902c96328a2bc38
BrousseWayne <samynedir@gmail.com>
2025-07-15 19:15:10 +0200

    add mock user and send mock token

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import cors from "cors";
+import { Pool } from "pg";
+import type { Request, Response } from "express";
 
 dotenv.config({ path: "/Users/samy/Projects/spiderBackend/conf.env" });
 
@@ -20,12 +22,31 @@ app.get("/", (req, res) => {
   res.send("LOGIN PLEASEEEE");
 });
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+const pool = new Pool();
 
-  //hash the password with a secure algorithm like bcrypt
-  // if valid => send a token
-  // else send ?
+app.post("/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+
+  console.log(result.rows);
+
+  if (result.rows.length === 0) {
+    res.status(401).json({ error: "Invalid credential" });
+    return;
+  }
+
+  const user = result.rows[0];
+  console.log(user.password === password);
+  if (user.password !== password) {
+    res.status(401).json({ error: "Invalid credential" });
+    return;
+  }
+
+  res.json({ token: "YEAHAHAHAHAHAHAHAHAHAHA" });
+  console.log("dit it wwww");
 });
 
 app.listen(PORT, () => {
